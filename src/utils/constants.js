0bc88902a8c4cbcc02ee34151ee3407e18dcee40
Mitzi <yriano69@gmail.com>
2025-11-19 15:06:18 -0600

    fix: set VUE_APP_API_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-
-export const API_URL = process.env.VUE_APP_STRAPI_URL || "http://localhost:1337";
+export const API_URL = process.env.VUE_APP_API_URL;
 export const TOKEN = "token";
 export const PRODUCTS = "PRODUCTS";

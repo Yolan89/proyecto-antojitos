604dfb5b8b32f62e19cd0615610152ff1fd4174f
Mitzi <yriano69@gmail.com>
2025-11-19 13:25:11 -0600

    fix: point API_URL to Heroku

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:1337";
+
+export const API_URL = process.env.VUE_APP_STRAPI_URL || "http://localhost:1337";
 export const TOKEN = "token";
 export const PRODUCTS = "PRODUCTS";
